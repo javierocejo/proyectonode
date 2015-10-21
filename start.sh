20696e27284944ae2d0970c7e09ef18e7e3a6883
Franco Laiuppa <franco.laiuppa@gmail.com>
2015-10-20 21:46:59 -0300

    Updated start.sh so it can run with forever and nodemon

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t javiocejo/node .
-docker run -it --rm --name testingwithbechi javiocejo/node
+docker run -it --rm --name testingwithbechi -v /var/www/proyectonode/app:/usr/src/myapp javiocejo/node forever -l log.txt -o out.txt -e err.txt --spinSleepTime 10000 --minUptime 5000 -c 'nodemon --exitcrash ' index.js
